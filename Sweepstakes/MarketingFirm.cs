41620a30dd806b4d6d3d4709902d12a676768d25
Lewis Vine <lew.vine@gmail.com>
2021-03-03 12:03:01 -0500

    Created MarketingFirm class

---
+++ Sweepstakes/MarketingFirm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class MarketingFirm
+    {
+        //Member variables
+        private ISweepstakesManager _manager;
+
+        //Constructor
+        public MarketingFirm(ISweepstakesManager manager)
+        {
+            _manager = manager;
+        }
+
+        //Member methods
+        public void CreateSweepstake(string name)
+        {
+            Sweepstakes sweepstake = new Sweepstakes(name);
+        }
+    }
+}
