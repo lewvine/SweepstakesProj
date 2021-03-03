cbe0ed13ef56503358b26c9b35a43ece20620fed
Lewis Vine <lew.vine@gmail.com>
2021-03-03 14:09:07 -0500

    Took logic out of user interface and created simulation class and placed logic in there

---
+++ Sweepstakes/Simulation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    public class Simulation
+    {
+        public void RunSimulation()
+        {
+            int managerType = UserInterface.ChooseSweepstakesManagerType();
+            SweepstakesManagerFactory(managerType);
+        }
+
+        public void SweepstakesManagerFactory(int userInput)
+        {
+            //Create a null instance.
+            ISweepstakesManager manager = null;
+
+            //Assign a derived class based on user input
+            if (userInput == 1)
+            {
+                manager = new SweepstakesStackManager();
+            }
+            else if (userInput == 2)
+            {
+                manager = new SweepstakesQueueManager();
+            }
+        }
+    }
+}
--- Sweepstakes/UserInterface.cs
+++ Sweepstakes/UserInterface.cs
@@ -6,30 +6,16 @@ using System.Threading.Tasks;
 
 namespace Sweepstakes
 {
-    class UserInterface
+    public static class UserInterface
     {
         //Methods
-        int userInput;
-        public void PromptType()
+        public static int ChooseSweepstakesManagerType()
         {
             Console.WriteLine("Welcome to Sweepstakes Manager!  Would you like to utlize " +
                 "a 'stack' or a 'queue' to keep track of your sweepstakes?  " +
                 "Type '1' for 'stack' or '2' for 'queue'");
-            userInput = Convert.ToInt32(Console.ReadLine());
+            return Convert.ToInt32(Console.ReadLine());
         }
-
-        public void SweepstakesManagerFactory()
-        {
-            ISweepstakesManager manager = null;
-            if (userInput == 1)
-            {
-                manager = new SweepstakesStackManager();
-            }
-            else if (userInput == 2)
-            {
-                manager = new SweepstakesQueueManager();
-            }
-        } 
     }
 
 }
