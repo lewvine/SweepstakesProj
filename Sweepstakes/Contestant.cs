4304abc1d29d2af01ce1b6d900efe002b82730a7
Lewis Vine <lew.vine@gmail.com>
2021-03-03 11:28:42 -0500

    Created and populated customer class with variables

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        //Member variables
+        public string FirstName;
+        public string LastName;
+        public string EmailAddress;
+        public int RegistrationNumber;
+
+        //Constructor
+        public Contestant(string first, string last, string email)
+        {
+            this.FirstName = first;
+            this.LastName = last;
+            this.EmailAddress = email;
+         }
+    }
+}
