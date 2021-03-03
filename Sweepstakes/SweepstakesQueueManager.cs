486f39c4258610f1dea219b351e5d84621b5655c
Lewis Vine <lew.vine@gmail.com>
2021-03-03 11:52:59 -0500

    Created and completed SweepstakesQueueManager class

---
+++ Sweepstakes/SweepstakesQueueManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class SweepstakesQueueManager
+    {
+        //Member variables
+        private Queue<Sweepstakes> queue;
+
+        //Constructor
+        public SweepstakesQueueManager(Queue<Sweepstakes> queue)
+        {
+            this.queue = queue;
+        }
+        //Member methods
+
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+            queue.Enqueue(sweepstakes);
+        }
+
+        public Sweepstakes GetSweepstakes()
+        {
+            return queue.Dequeue();
+        }
+    }
+}
