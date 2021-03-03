0c145fafde8c71f35c33b0d470bf3964616c0094
Lewis Vine <lew.vine@gmail.com>
2021-03-03 11:36:47 -0500

    Created Sweepstakes class with variables and shell methods

---
+++ Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Sweepstakes
+    {
+        //Member variables
+        private Dictionary<int, Contestant> contestants;
+        private string name;
+        public string Name { get; set;}
+
+        //Constructor
+        public Sweepstakes(string name)
+        {
+            this.name = name;        
+        }
+
+        //Member methods
+        public void RegisterContestant(Contestant contestant)
+        {
+            //DO SOMETHING
+        }
+
+        public Contestant PickWinner()
+        {
+            //DO SOMETHING
+        }
+
+        public void PrintContestantInfo(Contestant contestant)
+        {
+            Console.WriteLine($"");
+        }
+    }
+}
