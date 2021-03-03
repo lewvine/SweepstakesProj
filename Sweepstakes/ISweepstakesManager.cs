9fd2563b9e3f67aeace6eb06110c966d59ac5851
Lewis Vine <lew.vine@gmail.com>
2021-03-03 11:44:09 -0500

    Created Sweepstakes interface and populated with methods

---
+++ Sweepstakes/ISweepstakesManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    interface ISweepstakesManager
+    {
+        //No variables
+
+        //No constructors for intefaces
+
+        //Member methods
+
+        void InsertSweepstakes(Sweepstakes sweepstakes);
+
+        Sweepstakes GetSweepstakes();
+
+    }
+}
