1ccd47c2e3227d86b82f6a09d8912d4e9cbc9b5b
Lewis Vine <lew.vine@gmail.com>
2021-03-03 11:50:10 -0500

    Created and completed SweepstakesStackManager class

---
+++ Sweepstakes/SweepstakesStackManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class SweepstakesStackManager : ISweepstakesManager
+    {
+        //Member variables
+        private Stack<Sweepstakes> stack;
+
+        //Constructor
+        public SweepstakesStackManager(Stack<Sweepstakes> stack)
+        {
+            this.stack = stack;
+        }
+        //Member methods
+
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+            stack.Push(sweepstakes);
+        }
+
+        public Sweepstakes GetSweepstakes()
+        {
+            return stack.Pop();
+        }
+    }
+}
